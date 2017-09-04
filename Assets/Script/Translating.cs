d6eb77b081dfb1df3e86a41e88a81f2fc6b2bbf4
jpsiyu <jpsiyu@163.com>
2017-09-04 19:52:42 +0800

    add proj

---
+++ Assets/Script/Translating.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Translating : MonoBehaviour {
+
+    private float distance = 3.0f;
+    private float interval = 0.3f;
+    private float timeRecord = 0f;
+    private int r;
+
+	// Use this for initialization
+	void Start () {
+        timeRecord = 0f;
+        r = Random.Range(0, 4);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (timeRecord >= interval) {
+            Back();
+            timeRecord = 0f;
+            r = Random.Range(0, 4);
+        }
+        Move(r);
+        timeRecord += Time.deltaTime;
+    }
+
+    private void Move(int r) {
+        if (r == 0)
+            ToUp();
+        else if (r == 1)
+            ToDown();
+        else if (r == 2)
+            ToLeft();
+        else
+            ToRight();
+    }
+
+    private void ToRight() {
+        transform.localPosition = Vector3.Lerp(Vector3.zero, Vector3.right, interval);
+    }
+
+    private void ToLeft() {
+        transform.localPosition = Vector3.Lerp(Vector3.zero, Vector3.left, interval);
+    }
+
+    private void ToUp() {
+        transform.localPosition = Vector3.Lerp(Vector3.zero, Vector3.up, interval);
+    }
+
+    private void ToDown() {
+        transform.localPosition = Vector3.Lerp(Vector3.zero, Vector3.down, interval);
+    }
+
+    private void Back() {
+        transform.localPosition = Vector3.zero;
+    }
+}
