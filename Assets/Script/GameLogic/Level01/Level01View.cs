610fef0c2b4eeded8beaa10f8c19f0a59259fea1
jpsiyu <jpsiyu@163.com>
2017-11-06 11:42:07 +0800

    add born logic
    add born logic

--- Assets/Script/GameLogic/Level01/Level01View.cs
+++ Assets/Script/GameLogic/Level01/Level01View.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Tangzx.ABSystem;
 using UnityEngine;
 
 public class Level01View : ViewBase {
 
     private GameObject offset;
     private HoriMove horiMove;
+    private Transform bornPlace;
+    private GameObject player;
 
     private void Awake() {
         BindUI();
@@ -15,6 +18,7 @@ public class Level01View : ViewBase {
     private void BindUI() {
         offset = transform.Find("offset").gameObject;
         horiMove = offset.AddComponent<HoriMove>();
+        bornPlace = transform.Find("offset/born");
     }
 
     private void BindEvent() {
@@ -23,7 +27,24 @@ public class Level01View : ViewBase {
 
     private void Start() {
         horiMove.SetEndAction(delegate { Level01Ctrl.Close(); });
+        Run();
+    }
+
+    private void Run() {
+        Born();
         horiMove.StartMove();
     }
+
+    private void Born() {
+        AssetBundleManager.LoadAssetCompleteHandler handler = delegate (AssetBundleInfo abi) {
+            player = GameObject.Instantiate(abi.mainObject) as GameObject;
+            SimpleComponentUtil.SetParent(bornPlace, player.transform);
+        };
+        AssetBundleManager.Instance.Load(AssetPathDefinition.player, handler);
+    }
+
+    private void UnSafe() { }
+
+    private void Safe() { }
 
 }
